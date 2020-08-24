c50f6bdb4ee5c75a3ee05a7ce8fa24209d4f5d77
sarah <sarachamorro@gmail.com>
2020-08-24 17:53:26 -0300

    Figure out that youtube breakes with direct innerHTML injection

--- dist/contentScript.js
+++ dist/contentScript.js
@@ -1,9 +1,16 @@
 window.onload = () => {
-  document.body.innerHTML += `
-  <div class="modal-test">
-    <h1>THis is a test!</h1>
-  </div>
-  `
+  setTimeout(() => {
+    var div = document.createElement("div");
+    div.className = 'className';
+    div.innerHTML = `
+    <div class="modal-test">
+      <h1>THis is a test!</h1>
+    </div>
+    `;
+    // always use appendChild and not innerHTML directly in existing page elements, 
+    // otherwise it can make the page stop working
+    document.body.appendChild(div);
+  }, 5000);
 };
 
 // @sourceURL=contentScript.js 
